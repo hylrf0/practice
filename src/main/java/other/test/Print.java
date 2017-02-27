43b3e2a7d075892acc4be3cf2eff73fc3b54990d
吹哔战将 <hylrf0@126.com>
2017-02-27 19:35:57 +0800

    静态引入

---
+++ src/main/java/other/test/Print.java
@@ -0,0 +1,11 @@
+package other.test;
+
+/**
+ * Created by linrufeng on 2017/2/26.
+ */
+public class Print {
+
+    public static void print(String value){
+        System.out.println(value);
+    }
+}
---
+++ src/main/java/other/test/StaticImportTest.java
@@ -0,0 +1,13 @@
+package other.test;
+
+ import static other.test.Print.*;
+
+/**
+ * Created by linrufeng on 2017/2/26.
+ */
+public class StaticImportTest {
+
+    public static void main(String[] args) {
+        print("zxcv");
+    }
+}
